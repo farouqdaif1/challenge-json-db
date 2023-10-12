1a9bd511fd6a97e2f176af316460f8d95e9f5e7c
farouqdaif1 <daiffarouq@gmail.com>
2023-10-12 19:32:16 +0300

    feat: add function to delete file

--- helper/fileactions.js
+++ helper/fileactions.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const path = require('path')
 const folderPath = __dirname + '/../data/'
 
-async function createFile(studentId, fileContent) {
+async function createFile (studentId, fileContent) {
   const fileName = `${studentId}.json`
   const filePath = path.join(folderPath, fileName)
   fs.writeFile(filePath, JSON.stringify(fileContent), 'utf8', (err) => {
@@ -16,6 +16,20 @@ async function createFile(studentId, fileContent) {
       console.error('Error writing to the file:', err)
     } else {
       console.log('File has been written successfully.')
+    }
+  })
+}
+async function readFile () {
+
+}
+async function deleteFile(studentId) {
+  const fileName = `${studentId}.json`
+  const filePath = path.join(folderPath, fileName)
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(`Error deleting the file: ${err}`)
+    } else {
+      console.log(`File ${filePath} has been deleted.`)
     }
   })
 }
