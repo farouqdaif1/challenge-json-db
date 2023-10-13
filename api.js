7550e4155e2fa3ee866e8c771590463edcea95cd
farouqdaif1 <daiffarouq@gmail.com>
2023-10-13 20:02:19 +0300

    feat: add get end point

--- api.js
+++ api.js
@@ -15,17 +15,21 @@ async function putStudentData (req, res) {
   const postData = utils.createObject(req.params[0], req.body)
   try {
     const data = await fileHelper.createFile(studentId, postData)
-    
-    res.json({ message: 'Data created Successfully', data :data })
+
+    res.json({ message: 'Data created Successfully', data: data })
   } catch (error) {
     res.status(500).json({ message: error, status: false })
   }
 }
 async function getStudentData (req, res) {
+  const path = req.params[0]
+  const arrayPath = path.split(/\//)
   try {
     const { studentId } = req.params
     const data = await fileHelper.readFile(studentId)
-    res.json(data)
+    const result = arrayPath.reduce((accumulator, current) =>
+      accumulator && accumulator[current], data)
+    res.json(result)
   } catch (err) {
     res.status(404).send({ error: err.message })
   }
--- helper/util.js
+++ helper/util.js
@@ -1,19 +1,19 @@
 module.exports = {
-    createObject
+  createObject
 }
-function createObject(params, data) {
-    const nestedObject = {};
-    // Initialize a reference to the current object
-    let currentObject = nestedObject;
-    const keysList = params.split('/');
-    for (let i = 0; i < keysList.length - 1; i++) {
-        const key = keysList[i];
-        currentObject[key] = {};
-        currentObject = currentObject[key]
-    }
-    const finalKey = keysList[keysList.length - 1];
+function createObject (params, data) {
+  const nestedObject = {}
+  // Initialize a reference to the current object
+  let currentObject = nestedObject
+  const keysList = params.split('/')
+  for (let i = 0; i < keysList.length - 1; i++) {
+    const key = keysList[i]
+    currentObject[key] = {}
+    currentObject = currentObject[key]
+  }
+  const finalKey = keysList[keysList.length - 1]
 
-    currentObject[finalKey] = data;
+  currentObject[finalKey] = data
 
-    return nestedObject;
+  return nestedObject
 }
